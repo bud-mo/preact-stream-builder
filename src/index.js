67e9cd3371aceb9c9b086f75d9c4d22acdb9dd45
Michele Olivo <set>
2019-06-09 23:11:03 +0200

    First commit, created a StreamBuilder inspired by Flutter.

---
+++ src/index.js
@@ -0,0 +1,40 @@
+import { h, Component } from 'preact';
+import { Subscription } from 'rxjs';
+
+export const ConnectionState = {
+    active: 'active',
+    done: 'done',
+    none: 'none',
+    values: 'values',
+    waiting: 'waiting'
+};
+
+export default class StreamBuilder extends Component {
+
+    constructor(props) {
+        this._subscription = Subscription.EMPTY;
+
+        this.state = {
+            data: props.initialData,
+            state: ConnectionState.none
+        };
+    }    
+
+    componentDidMount() {
+        this.setState(() => ({ state: ConnectionState.waiting }));
+        this._subscription = this.props.stream.subscribe({
+            next: (data) => this.setState(() => ({ data, state: ConnectionState.active })),
+            error: () => {},
+            complete: () => this.setState(() => ({ state: ConnectionState.done }))
+        });
+    }
+
+    componentWillUnmount() {
+        this._subscription.unsubscribe();
+    }
+
+    render({ builder }, snapshot) {
+        return builder(snapshot);
+    }
+
+}
