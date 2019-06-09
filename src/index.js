f97fdd23f8b9efcf1196c77c30ff6ff4462f059e
Michele Olivo <set>
2019-06-10 00:01:01 +0200

    Playing with microbundle configuration

--- src/index.js
+++ src/index.js
@@ -1,40 +1,42 @@
+/* eslint-disable react/no-did-mount-set-state */
 import { Component } from 'preact';
 import { Subscription } from 'rxjs';
 
 export const ConnectionState = {
-    active: 'active',
-    done: 'done',
-    none: 'none',
-    values: 'values',
-    waiting: 'waiting'
+	active: 'active',
+	done: 'done',
+	none: 'none',
+	values: 'values',
+	waiting: 'waiting'
 };
 
 export class StreamBuilder extends Component {
 
-    constructor(props) {
-        this._subscription = Subscription.EMPTY;
+	constructor(props) {
+		super(props);
+		this._subscription = Subscription.EMPTY;
 
-        this.state = {
-            data: props.initialData,
-            state: ConnectionState.none
-        };
-    }    
+		this.state = {
+			data: props.initialData,
+			state: ConnectionState.none
+		};
+	}
 
-    componentDidMount() {
-        this.setState(() => ({ state: ConnectionState.waiting }));
-        this._subscription = this.props.stream.subscribe({
-            next: (data) => this.setState(() => ({ data, state: ConnectionState.active })),
-            error: () => {},
-            complete: () => this.setState(() => ({ state: ConnectionState.done }))
-        });
-    }
+	componentDidMount() {
+		this.setState(() => ({ state: ConnectionState.waiting }));
+		this._subscription = this.props.stream.subscribe({
+			next: (data) => this.setState(() => ({ data, state: ConnectionState.active })),
+			error: () => {},
+			complete: () => this.setState(() => ({ state: ConnectionState.done }))
+		});
+	}
 
-    componentWillUnmount() {
-        this._subscription.unsubscribe();
-    }
+	componentWillUnmount() {
+		this._subscription.unsubscribe();
+	}
 
-    render({ builder }, snapshot) {
-        return builder(snapshot);
-    }
+	render({ builder }, snapshot) {
+		return builder(snapshot);
+	}
 
 }
